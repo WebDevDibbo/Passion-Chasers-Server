875d71cd906950f3bec4912cdefb32d138840ebd
dibbodas12 <dibbodas228@gmail.com>
2022-10-25 22:33:58 +0600

    data updated

--- index.js
+++ index.js
@@ -7,12 +7,17 @@ const port = process.env.PORT || 5000;
 app.use(cors());
 
 const courseCategories = require('./data/categories.json')
+const courses = require('./data/courses.json');
+const { application } = require('express');
 
 app.get('/', (req, res) => {
     res.send('Course Api running');
 })
 app.get('/course-categories', (req, res) => {
     res.send(courseCategories)
+})
+app.get('/courses',(req,res)=>{
+    res.send(courses)
 })
 app.listen(port,()=> {
     console.log('Courses running on port ', port)
