1ece7b9166134be1b83fdc7903b14edb9f31d378
dibbodas12 <dibbodas228@gmail.com>
2022-10-25 19:18:32 +0600

    cors install and added

--- index.js
+++ index.js
@@ -1,7 +1,10 @@
-const { application } = require('express');
+// const { application } = require('express');
 const express = require('express');
 const app = express();
+const cors = require('cors');
 const port = process.env.PORT || 5000;
+
+app.use(cors());
 
 const courseCategories = require('./data/categories.json')
 
